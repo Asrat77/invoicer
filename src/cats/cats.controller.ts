254335182409aea9764ed3387df2c8c09c79c2cd
Asrat <asratextras77@gmail.com>
2024-12-08 20:22:37 +0300

    cats

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Post, Param, Body } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { Cat } from './interfaces/cat.interface';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  show(@Param('id') id: string): string {
+    return `Cat with id: #${id}`;
+  }
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+}
